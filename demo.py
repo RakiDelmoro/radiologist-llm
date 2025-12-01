c72b5cb0e140eabe495be522bdbce2782902e341
Raki Delmoro <rakidelmoro@gmail.com>
2025-12-01 13:47:54 +0000

    Investigate why it takes too long to run the model

--- demo.py
+++ demo.py
@@ -1,14 +1,11 @@
 import torch
 from PIL import Image
-from transformers import AutoProcessor
 from unsloth import FastVisionModel
+from transformers import AutoProcessor
 
 def run_model_demo():
-    model, tokenizer = FastVisionModel.from_pretrained(
-    model_name="qwen3vl-2b",
-    load_in_4bit=True,
-    use_gradient_checkpointing="unsloth")
-    FastVisionModel.for_inference(model)
+    model, tokenizer = FastVisionModel.from_pretrained("Qwen3-vl-2B", load_in_4bit=True, local_files_only=True)
+    print('Finish preparing model')
 
     image_path = "./demo-pic.JPG"
     image = Image.open(image_path).convert('RGB')
@@ -21,12 +18,18 @@ def run_model_demo():
         ]
     },
     ]
-
+    print('Finish preparing messages.')
+    
     text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-    processor = AutoProcessor.from_pretrained("qwen3vl-2b")
+    processor = AutoProcessor.from_pretrained("Qwen3-vl-2B")
     inputs = processor(text=text, images=[image], return_tensors="pt").to(model.device)
+    print('Finish preparing inputs for model')
+    
     with torch.inference_mode(): output = model.generate(**inputs, max_new_tokens=512, do_sample=False)
+    print('Finish generating answer from the model')
+
     answer = tokenizer.decode(output[0], skip_special_tokens=True)
     print(answer)
+    print('Finish decode answer')
 
 run_model_demo()
