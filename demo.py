1989ec1948dda85d84fc52c46d4f3fa4f64c5041
Raki Delmoro <rakidelmoro@gmail.com>
2025-12-08 23:18:34 +0000

    test finetune model checkpoint

--- demo.py
+++ demo.py
@@ -1,35 +1,57 @@
+import time
 import torch
 from PIL import Image
+from datasets import load_dataset
 from unsloth import FastVisionModel
 from transformers import AutoProcessor
 
 def run_model_demo():
-    model, tokenizer = FastVisionModel.from_pretrained("Qwen3-vl-2B", load_in_4bit=True, local_files_only=True)
-    print('Finish preparing model')
+    start = time.time()
+    # image_path = "./output_image.jpg"
+    # image = Image.open(image_path)
 
-    image_path = "./demo-pic.JPG"
-    image = Image.open(image_path).convert('RGB')
+    dataset = load_dataset('itsanmolgupta/mimic-cxr-dataset')
+    image = dataset['train']['image'][-4]
+    findings = dataset['train']['findings'][-4]
+    impression = dataset['train']['impression'][-4]
+    # Save image locally for visualization
+    image.save("output_image.jpg")
+
+    model, tokenizer = FastVisionModel.from_pretrained('x-ray-agent-v2-checkpoints/checkpoint-378', local_files_only=True)
+
+    instruction = """Describe this X-ray."""
     messages = [
+    {
+        "role": "system",
+        "content": [
+            {"type": "text", "text": "You are an expert radiologist."}
+        ]
+    },
     {
         "role": "user",
         "content": [
-            {"type": "image", "url": image_path},
-            {"type": "text", "text": "What animal is on the candy?"}
+            {"type": "image", "image": image},
+            {"type": "text", "text": instruction}
         ]
-    },
+    }
     ]
-    print('Finish preparing messages.')
-    
-    text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-    processor = AutoProcessor.from_pretrained("Qwen3-vl-2B")
-    inputs = processor(text=text, images=[image], return_tensors="pt").to(model.device)
-    print('Finish preparing inputs for model')
-    
-    with torch.inference_mode(): output = model.generate(**inputs, max_new_tokens=512, do_sample=False)
-    print('Finish generating answer from the model')
 
-    answer = tokenizer.decode(output[0], skip_special_tokens=True)
+    input_text = tokenizer.apply_chat_template(messages, add_generation_prompt = True)
+    inputs = tokenizer(
+        image,
+        input_text,
+        add_special_tokens = False,
+        return_tensors = "pt",
+    ).to("cuda")
+
+    with torch.inference_mode(): output = model.generate(**inputs, max_new_tokens=512, do_sample=False)
+    generated_tokens = output[0][inputs['input_ids'].shape[1]:]
+    answer = tokenizer.decode(generated_tokens, skip_special_tokens=True)
     print(answer)
-    print('Finish decode answer')
+    end = time.time()
+    print(f'Finish in {end - start:.2f} seconds')
+
+    print(findings)
+    print(impression)
 
 run_model_demo()
